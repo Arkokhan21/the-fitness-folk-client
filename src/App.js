6c9fa2849b8a66cd358499575c723723dcb9676c
Arkokhan21 <khanarko2021@gmail.com>
2022-11-08 01:07:00 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <div className="navbar bg-slate-300 p-3">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link>Item 1</Link></li>
+                        <li><Link>Blog</Link></li>
+                        <li> <Link className="btn btn-active btn-accent text-white">Login</Link></li>
+                    </ul>
+                </div>
+                <Link className="text-xl font-bold">The Fitness Folk</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0 font-semibold">
+                    <li><Link>Item 1</Link></li>
+                    <li><Link className='ml-5'>Blog</Link></li>
+                    <li><Link className="btn btn-active btn-accent text-white ml-5">Login</Link></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
